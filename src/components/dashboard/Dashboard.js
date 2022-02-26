632e047ee3c5b7e5e7ca4d6149f9c37403eeead7
Kevin Osborn <kevinosborn@kevins-mbp.lan>
2022-02-26 20:00:47 +1100

    Connected to real time data API

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -44,6 +44,44 @@ function Dashboard() {
         apiCall()
       },[])
 
+      useEffect(() => {
+        if (!first.current) {
+            return;
+        }
+
+        let msg = {
+            type: "subscribe",
+            product_ids: [pair],
+            channels: ["ticker"]
+        };
+
+        let jsonMsg = JSON.stringify(msg);
+        ws.current.send(jsonMsg);
+
+        let historicalDataURL = `${url}/products/${pair}/candles?granularity=86400`;
+        const fetchHistoricalData = async () => {
+            let dataArr = [];
+            await fetch(historicalDataURL)
+                .then((res) => res.json())
+                .then((data) => (dataArr = data));
+
+                let formattedData = formatData(dataArr);
+                setPastData(formattedData);
+        };
+        // run async function to get historical data
+        fetchHistoricalData();
+
+        ws.current.onmessage = (e) => {
+            let data = JSON.parse(e.data);
+            if (data.type !== "ticker") {
+                return;
+            }
+            if (data.product_id === pair) {
+                setPrice(data.price);
+            }
+        };
+      }, [pair]);
+
 
 
     return (
