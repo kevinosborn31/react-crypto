401158371509a279ca95bd7dbdb73399c6cb9232
Kevin Osborn <kevinosborn@kevins-mbp.lan>
2022-02-26 19:52:17 +1100

    Moved getData util function directly into dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,5 +1,4 @@
-import React, {useState, useEffect} from "react";
-import getData from "../../utils/getData";
+import React, {useState, useEffect, useRef} from "react";
 
 function Dashboard() {
 
@@ -8,9 +7,41 @@ function Dashboard() {
     const [price, setPrice] = useState("0.00");
     const [pastData, setPastData] = useState({});
 
+    const ws = useRef(null);
+    let first = useRef(false);
+    const url = "https://api.pro.coinbase.com";
+
     useEffect(() => {
-        getData("USD");
-    }, [])
+        ws.current = new WebSocket("wss://ws-feed.pro.coinbase.com");
+
+        const apiCall = async () => {
+            await fetch(url + "/products")
+              .then((res) => res.json())
+              .then((data) => (pairs = data));
+        
+            let filtered = pairs.filter((pair) => {
+              if (pair.quote_currency === "USD") {
+                return pair;
+              }
+            });
+
+            filtered = filtered.sort((a, b) => {
+              if (a.base_currency < b.base_currency) {
+                return -1;
+                }
+              if (a.base_currency > b.base_currency) {
+                return 1;
+                }
+              return 0;
+            });
+          
+          setCurrencies(filtered);
+      
+          first.current = true;
+        };
+        
+        apiCall()
+      },[])
 
 
 
